9f30c8808a883e3f7d1a8061ba6f59fd17a22a32
Daniel Morao <13-10932@usb.ve>
2020-05-20 21:34:53 -0400

    Previo a conectarse al server

--- server/server.js
+++ server/server.js
@@ -1,3 +1,25 @@
+require("dotenv").config();
+const cors = require("cors");
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+
+// Puertos y URL
+const uri = process.env.ATLAS_URI;
+const PORT = process.env.PORT;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connetc to ATLAS
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB Atlas ..."))
+  .catch((err) => console.error("Could not connect to MongoDB !!!"));
+
+app.listen(PORT, console.log(`Listening on port ${PORT}`));
